ef47304e17d10f0c31d40ca84dc9860985263195
21x37 <tylermok@outlook.com>
2019-04-23 20:43:06 -0700

    added about page

---
+++ src/components/about/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../header/Header';
+
+const About = () => {
+    return (
+        <div>
+            <Header />
+            About
+        </div>
+    );
+};
+
+export default About;
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,6 +2,7 @@ import React from 'react';
 import createHistory from 'history/createBrowserHistory';
 import { Router, Route, Switch } from 'react-router-dom';
 import Index from '../components/Index';
+import About from '../components/about/About';
 
 
 const history = createHistory();
@@ -11,6 +12,7 @@ const AppRouter = () => (
         <div>
            <Switch>
                 <Route path='/' component={Index} exact={true}/>
+                <Route path='/about' component={About} exact={true}/>
            </Switch> 
         </div>
     </Router>
